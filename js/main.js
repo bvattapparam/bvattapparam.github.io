a0553ef4f6c6a201a69c78c5d5627e6d30ccf1be
bvattapparam <bijeshkumarvattapparambath@Bijeshkumars-MacBook-Pro.local>
2019-08-24 12:09:46 +0530

    changes the cache

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ if('serviceWorker' in navigator){
     console.log('Service worker: supported.')
     window.addEventListener('load', () => {
         navigator.serviceWorker
-        .register('../sw_cached_site.js')
+        .register('../sw_cached_pages.js')
         .then(reg => console.log('Service worker: Registered.'))
         .catch(err => console.log('Service worker: Error: ${err}'));
     });
